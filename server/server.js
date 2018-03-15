90988e9c7da8af49da85a6d11e1105aff5002058
mulakalaveena <m.veena2k14@gmail.com>
2018-03-15 16:47:33 +0530

    git commit

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const publicPath = path.join(__dirname,'../public')
 const express=require('express')
 const app=express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 console.log('hello')
 console.log(publicPath)
